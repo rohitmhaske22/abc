0a832e12136e54c289c0ac7215407101ce04ef7e
rohitmhaske22 <rohitmhaske900@gmail.com>
2025-01-12 13:58:21 +0530

    Project for git uploaded

---
+++ Project_for_git/Project for git/Controllers/ABCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_for_git.Controllers
+{
+    public class ABCController : Controller
+    {
+        // GET: ABC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project_for_git/Project for git/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_for_git.MvcApplication" Language="C#" %>
